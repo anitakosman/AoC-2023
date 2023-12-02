0b6cdfb0833789cedb020468ab7cb96b285b75b0
Anita <anitakosman@hotmail.com>
2023-12-02 12:11:27 +0100

    Add day 2

---
+++ src/day2.rs
@@ -0,0 +1,44 @@
+use std::cmp::max;
+use std::collections::HashMap;
+use std::fs;
+
+pub fn run() {
+    let input = fs::read_to_string("src/resources/input2").expect("Should have been able to read the file");
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
+}
+
+fn part1(input: &String) -> u32 {
+    let available_cubes = HashMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14)
+    ]);
+
+    input.lines().enumerate().map(|(game, line)| {
+        if line.split(": ").nth(1).unwrap_or_default().split("; ").all(|set| {
+            set.split(", ").all(|cubes| {
+                let mut split = cubes.split(" ");
+                let n = split.next().and_then(|s| s.parse::<u32>().ok()).unwrap_or_default();
+                let color = split.next().unwrap_or_default();
+                available_cubes.get(color).unwrap_or(&0) >= &n
+            })
+        }) { game as u32 + 1 } else { 0 }
+    }).sum()
+}
+
+fn part2(input: &String) -> u32 {
+    input.lines().map(|line| {
+        let mut cubes_per_color = HashMap::new();
+        for set in line.split(": ").nth(1).unwrap_or_default().split("; ") {
+            for cubes in set.split(", ") {
+                let mut split = cubes.split(" ");
+                let n = split.next().and_then(|s| s.parse::<u32>().ok()).unwrap_or_default();
+                let color = split.next().unwrap_or_default();
+                let current_max = cubes_per_color.get(color).unwrap_or(&0);
+                cubes_per_color.insert(color, max(*current_max, n));
+            }
+        }
+        cubes_per_color.values().product::<u32>()
+    }).sum()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,11 @@
-use std::fs;
-use regex::Regex;
-
-struct Digit {
-    name: String,
-    value: u32,
-}
+mod day1;
+mod day2;
 
 fn main() {
-    let contents = fs::read_to_string("src/resources/input1").expect("Should have been able to read the file");
-    println!("{}", calibrations(contents.clone()).iter().sum::<u32>());
-    println!("{}", calibrations2(contents.clone()).iter().sum::<u32>());
-}
-
-fn calibrations(input: String) -> Vec::<u32> {
-    let mut result = Vec::new();
-    let re = Regex::new(r"\d").unwrap();
-    for line in input.lines() {
-        let mut matches = re.find_iter(line);
-        let a = matches.next().unwrap().as_str().parse::<u32>().unwrap();
-        let b = matches.last().and_then(|m| Some(m.as_str())).and_then(|s| Some(s.parse::<u32>().unwrap())).unwrap_or(a);
-        result.push(a * 10 + b);
+    let day = std::env::args().nth(1).and_then(|s| s.parse::<u32>().ok()).unwrap_or(0);
+    match day {
+        1 => { day1::run(); }
+        2 => { day2::run(); }
+        _ => { println!("Please specify which day as command line arg") }
     }
-    result
-}
-
-fn calibrations2(input: String) -> Vec::<u32> {
-    let digits = vec![
-        Digit { name: String::from("one"), value: 1 },
-        Digit { name: String::from("two"), value: 2 },
-        Digit { name: String::from("three"), value: 3 },
-        Digit { name: String::from("four"), value: 4 },
-        Digit { name: String::from("five"), value: 5 },
-        Digit { name: String::from("six"), value: 6 },
-        Digit { name: String::from("seven"), value: 7 },
-        Digit { name: String::from("eight"), value: 8 },
-        Digit { name: String::from("nine"), value: 9 },
-    ];
-
-    let mut result = Vec::new();
-    let all_digits = digits.iter().map(|d| d.name.as_str()).collect::<Vec<&str>>().join("|");
-    let r = Regex::new(&*format!(r"\d|{}", all_digits)).unwrap();
-    let r2 = Regex::new(&*format!(r"\d|{}", reverse(all_digits))).unwrap();
-    for line in input.lines() {
-        let m1 = r.find(line).unwrap().as_str();
-        let reversed_line = &*reverse(String::from(line));
-        let m2 = reverse(String::from(r2.find(reversed_line).unwrap().as_str()));
-        let a = digits.iter().find(|d| d.name == m1 || d.value.to_string() == m1).unwrap_or_else(|| {println!("{}", line); panic!("")}).value;
-        let b = digits.iter().find(|d| d.name == m2 || d.value.to_string() == m2).unwrap_or_else(|| {println!("{}", line); panic!("")}).value;
-        result.push(a * 10 + b);
-    }
-    result
-}
-
-fn reverse(s: String) -> String {
-    s.chars().rev().collect()
 }
