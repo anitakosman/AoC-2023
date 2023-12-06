b032d0cdf6794c8d1e517238f80bb8104c0e1a3d
Anita <anitakosman@hotmail.com>
2023-12-06 09:06:59 +0100

    Add day 6

---
+++ src/day06.rs
@@ -0,0 +1,51 @@
+use std::fs;
+
+pub fn run() {
+    let input = fs::read_to_string("src/resources/input06").expect("Should have been able to read the file");
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
+}
+
+#[test]
+fn test() {
+    let input = fs::read_to_string("src/resources/test06").expect("Should have been able to read the file");
+    assert_eq!(part1(&input), 288);
+    assert_eq!(part2(&input), 71503);
+}
+
+fn part1(input: &String) -> u64 {
+    let mut lines = input.lines();
+    let ts = get_numbers_from_line(lines.next().unwrap());
+    let ds = get_numbers_from_line(lines.next().unwrap());
+    let races = ts.iter().zip(ds.iter());
+    races.map(|(t, d)| {
+        let range = get_win_range(*t, *d);
+        range
+    }).product()
+}
+
+fn part2(input: &String) -> u64 {
+    let mut lines = input.lines();
+    let ts = get_numbers_from_line(lines.next().unwrap()
+        .strip_prefix("Time: ").unwrap()
+        .replace(" ", "").as_str());
+    let ds = get_numbers_from_line(lines.next().unwrap()
+        .strip_prefix("Distance: ").unwrap()
+        .replace(" ", "").as_str());
+    get_win_range(*ts.get(0).unwrap(), *ds.get(0).unwrap())
+}
+
+fn get_win_range(t: u64, d: u64) -> u64 {
+    let sqrt = ((t * t - 4 * d) as f64).sqrt();
+    let max_float = (t as f64 + sqrt) / 2.0;
+    let min_float = (t as f64 - sqrt) / 2.0;
+    let max_x = if max_float.floor() == max_float {max_float as u64 - 1} else {max_float.floor() as u64};
+    let min_x = if min_float.ceil() == min_float {min_float as u64 + 1} else {min_float.ceil() as u64};
+    max_x - min_x + 1
+}
+
+fn get_numbers_from_line(line: &str) -> Vec<u64> {
+    line.split(" ")
+        .filter_map(|s| { s.parse::<u64>().ok() })
+        .collect()
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
 fn main() {
     let day = std::env::args().nth(1).and_then(|s| s.parse::<u32>().ok()).unwrap_or(0);
@@ -12,6 +13,7 @@ fn main() {
         3 => { day03::run(); }
         4 => { day04::run(); }
         5 => { day05::run(); }
+        6 => { day06::run(); }
         _ => { println!("Please specify which day as command line arg") }
     }
 }
